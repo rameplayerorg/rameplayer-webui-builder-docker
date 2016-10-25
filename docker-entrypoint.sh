036b2aa2843212042f1115f41b473b40c0957c62
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2016-10-25 18:03:17 +0300

    execute npm and bower install always before build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,10 @@ set -e
 
 if [ "$1" = 'build' ]; then
 	cd /rameplayer-webui
+	# make sure all needed npm and bower packages are installed
+	npm install
+	bower install --allow-root
+	# start building using gulp
 	gulp build
 	# move files to build directory defined in Dockerfile
 	cp -R build/* $BUILD_DIR
